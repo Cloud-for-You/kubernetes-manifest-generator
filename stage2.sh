a314bb29459b0c53f30c2b7d6f2ffed67b91e07b
oaz-dev <oaz-dev@localhost.localdomain>
2020-10-19 02:47:48 +0000

    WIP

--- stage2.sh
+++ stage2.sh
@@ -7,6 +7,13 @@ set -e -u
 CLUSTER_DIR="$(pwd)"
 
 git reset HEAD
+git submodule update --remote script/
+git add script
+if ! git diff --cached --exit-code &>/dev/null; then
+  git commit -m "Updated scripts submodule"
+fi
+
+git reset HEAD
 git add values custom
 if ! git diff --cached --exit-code &>/dev/null; then
   git commit -m "Cluster configuration"
