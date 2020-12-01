1fbe90eb5327f77690e9bb1a0a662ad8611ae409
poc-ocp4-cluster@ipocpbas01.vs.csin.cz <openshift@csas.cz>
2020-12-01 16:00:06 +0100

    Call function for deploy ArgoCD-SYS

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-oc create -f resources/cluster-config/MachineConfigs/
+#for file in $(find resources/cluster-config/MachineConfigs/ -type f); do
+#  oc create --save-config -f ${file} > /dev/null 2>&1 
+#  echo $?
+#done
+
+oc apply -f resources/cluster-config/MachineConfigs/ > /dev/null 2>&1
+
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+NC=`tput sgr0`        # No Color
+
+ROOTDIR=$(pwd)
+
+for function_file in `find $(dirname $0)/function/ -name *.func`; do
+  . ${function_file}
+done
+
+deploy_argosys
