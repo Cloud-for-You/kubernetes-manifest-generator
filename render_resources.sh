03bcc76fcb097859812a41d30a0ff5c01969aa8b
Petr Hatoň <70328252+hates52@users.noreply.github.com>
2023-12-15 20:27:18 +0100

    Zakomentovani potvrzovani

--- render_resources.sh
+++ render_resources.sh
@@ -27,38 +27,38 @@ clean
 getComponents
 renderBootstrap
 
-# Provedeme GIT commit
-while true; do
-  read -p "${GREEN}Commit .FINAL_RESOURCES changes?${NC} [y/n] " yn
-  case $yn in
-    [Yy]* )
-      prepareVersion
-      break
-      ;;
-    [Nn]* )
-      echo "Commit nebyl proveden"
-      exit 0
-      ;;
-    * )
-      echo "${RED}Please answer yes or no.${NC}"
-      ;;
-  esac
-done
+## Provedeme GIT commit
+#while true; do
+#  read -p "${GREEN}Commit .FINAL_RESOURCES changes?${NC} [y/n] " yn
+#  case $yn in
+#    [Yy]* )
+#      prepareVersion
+#      break
+#      ;;
+#    [Nn]* )
+#      echo "Commit nebyl proveden"
+#      exit 0
+#      ;;
+#    * )
+#      echo "${RED}Please answer yes or no.${NC}"
+#      ;;
+#  esac
+#done
 
-# Provedeme release do GITu
-while true; do
-  read -p "${GREEN}Release .FINAL_RESOURCES to cluster?${NC} [y/n] " yn
-  case $yn in
-    [Yy]* )
-      releaseVersion
-      break
-      ;;
-    [Nn]* )
-      echo "Verze nebude nasazena na cluster"
-      exit 0
-      ;;
-    * )
-      echo "${RED}Please answer yes or no.${NC}"
-      ;;
-  esac
-done
+## Provedeme release do GITu
+#while true; do
+#  read -p "${GREEN}Release .FINAL_RESOURCES to cluster?${NC} [y/n] " yn
+#  case $yn in
+#    [Yy]* )
+#      releaseVersion
+#      break
+#      ;;
+#    [Nn]* )
+#      echo "Verze nebude nasazena na cluster"
+#      exit 0
+#      ;;
+#    * )
+#      echo "${RED}Please answer yes or no.${NC}"
+#      ;;
+#  esac
+#done
